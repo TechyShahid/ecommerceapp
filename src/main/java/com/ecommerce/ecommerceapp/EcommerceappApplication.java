f5fa62b957c215b456a21280067905f92bc0d1f5
techyshahid <geekshahid@gmail.com>
2025-01-16 22:57:28 +0530

    committing code skeleton

---
+++ src/main/java/com/ecommerce/ecommerceapp/EcommerceappApplication.java
@@ -0,0 +1,14 @@
+package com.ecommerce.ecommerceapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceappApplication {
+
+	public static void main(String[] args) {
+		//entry point
+		SpringApplication.run(EcommerceappApplication.class, args);
+	}
+
+}
