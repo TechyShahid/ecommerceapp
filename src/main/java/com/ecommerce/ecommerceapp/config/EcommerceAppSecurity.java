c5a6e3149e7e9a301aa9758fcfd1a986b41adbc4
techyshahid <geekshahid@gmail.com>
2025-01-18 23:53:55 +0530

    committing changes related to spring security

---
+++ src/main/java/com/ecommerce/ecommerceapp/config/EcommerceAppSecurity.java
@@ -0,0 +1,11 @@
+package com.ecommerce.ecommerceapp.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class EcommerceAppSecurity{
+
+}
