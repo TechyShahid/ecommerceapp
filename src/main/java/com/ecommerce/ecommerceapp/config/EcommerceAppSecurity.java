9ced0ccf9c16696305a3ba1b2a75d9a60bbbf950
techyshahid <geekshahid@gmail.com>
2025-01-20 13:24:53 +0530

    committing changes related to spring security 2

--- src/main/java/com/ecommerce/ecommerceapp/config/EcommerceAppSecurity.java
+++ src/main/java/com/ecommerce/ecommerceapp/config/EcommerceAppSecurity.java
@@ -1,11 +1,55 @@
 package com.ecommerce.ecommerceapp.config;
 
 
+import com.ecommerce.ecommerceapp.dao.UserDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
-@EnableWebSecurity
 public class EcommerceAppSecurity{
+    @Autowired
+    UserDAO userDAO;
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/getAllUser").permitAll()
+                        .requestMatchers("/userAuthentication","/deleteAllUser").authenticated()
+                ).httpBasic(withDefaults());
+        return http.build();
+    }
+    // User Creation
+    //@Bean
+    public UserDetailsService userDetailsService(PasswordEncoder encoder) {
 
+        // InMemoryUserDetailsManager setup with two users
+        UserDetails admin = User.withUsername("Shahid")
+                .password(encoder.encode("123"))
+                .roles("ADMIN", "USER")
+                .build();
+
+        UserDetails user = User.withUsername("Shahid")
+                .password(encoder.encode("123"))
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(admin, user);
+    }
+    // Password Encoding
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
---
+++ src/main/java/com/ecommerce/ecommerceapp/entity/UserDetails.java
@@ -0,0 +1,15 @@
+package com.ecommerce.ecommerceapp.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDetails {
+    private String user;
+    private String password;
+}
